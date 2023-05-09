c84b3435933a7c560f5cb68e3775e1245fef8ab9
Woochal <112460787+Woochal@users.noreply.github.com>
2023-05-10 00:04:37 +0900

    conflict resolving test

--- util.h
+++ util.h
@@ -29,7 +29,12 @@
 void selectMode()
 {
     int mode;
-    printf("0. Manager Mode\t1.User Mode : ");
+
+    printf("\n******맘스카페에 오신 것을 환영합니다!******\n\n");
+    printf("----------------------------\n");
+    printf("0. 매니저 모드 \n1. 유저모드 \n");
+    printf("----------------------------\n");
+    printf("\n원하시는 모드를 입력해 주세요 => ");
     scanf("%d", &mode);
     if (mode)
         userMode();
