f2cf4196e49eebe6b1c808c1fd1e96b698175f69
Woochal <112460787+Woochal@users.noreply.github.com>
2023-05-16 02:41:38 +0900

    loadData into user.h

--- user.h
+++ user.h
@@ -150,6 +150,28 @@ void userUpdateShoppingBasket(ShoppingBasket *b, int *basketCount)
         *basketCount -= 1;
     }
 }
+int loadData(Menu *s)
+{
+    FILE *fp = NULL;
+    fp = fopen("menu.txt", "rt");
+    if (fp == NULL)
+    {
+        printf("No file!\n");
+        return 0;
+    }
+    int i = 0;
+
+    while (!feof(fp))
+    {
+        fgets(s[i].name, sizeof(s[i].name), fp);
+        s[i].name[strlen(s[i].name) - 1] = '\0';
+        fscanf(fp, "%c\n%d\n", &s[i].size, &s[i].price);
+        i++;
+    }
+    fclose(fp);
+    printf("==> Loading Success!\n");
+    return i;
+}
 void Pay() {}
 void usingCoupon() {}
 void purchase() {}
--- util.h
+++ util.h
@@ -4,34 +4,11 @@
 #include "manager.h"
 #include "user.h"
 
-// int loadData(Menu *s)
-// {
-//     FILE *fp = NULL;
-//     fp = fopen("menu.txt", "rt");
-//     if (fp == NULL)
-//     {
-//         printf("No file!\n");
-//         return 0;
-//     }
-//     int i = 0;
-
-//     while (!feof(fp))
-//     {
-//         fgets(s[i].name, sizeof(s[i].name), fp);
-//         s[i].name[strlen(s[i].name) - 1] = '\0';
-//         fscanf(fp, "%c\n%d\n", &s[i].size, &s[i].price);
-//         i++;
-//     }
-//     fclose(fp);
-//     printf("==> Loading Success!\n");
-//     return i;
-// }
-
 void selectMode()
 {
     int mode;
 
-    printf("\n******맘스카페에 오신 것을 환영합니다!******\n\n");
+    printf("******맘스카페에 오신 것을 환영합니다!******\n\n");
     printf("----------------------------\n");
     printf("0. 매니저 모드 \n1. 유저모드 \n");
     printf("----------------------------\n");
